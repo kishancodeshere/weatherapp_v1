bd92f14b089aaf3cbd295ab40e8096a550efed02
Kishan <kishan09101@gmail.com>
2023-03-09 14:27:43 +0530

    Fix frontend UI

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
